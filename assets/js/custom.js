3caf63a68a35228abab28fb08a4192762cb65f82
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2021-03-18 08:27:51 +0700

    push

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
+  $(window).scroll(function() {
+    var thisScroll = $(this).scrollTop()
+    if(thisScroll>0){
+      $(".navbar-light").addClass('scrolled')
+    }
+    else{
+      $(".navbar-light").removeClass('scrolled')
+    }
+  })
 });
---
+++ assets/js/destinations.js
@@ -0,0 +1,217 @@
+const destinations = [
+/*     {
+        "name" : "Kawah Ijen",
+        "banner" : "Kawah-Ijen.png",
+        "position" : "left:40%;top:0%;transform: translateY(-50%);-moz-transform: translateY(-50%);-webkit-transform: translateY(-50%);",
+    },
+    {
+        "name" : "Megasari",
+        "banner" : "Megasari.png",
+        "position" : "left:86%;top:10%",
+    },
+    {
+        "name" : "Arak Arak",
+        "banner" : "Arak-Arak.png",
+        "position" : "left:65%;top:20%",
+    }
+ */
+    {
+        "name" : "Coffee Plantation",
+        "pinImg" : "Arak-Arak.png",
+        "position" : "left:65%;top:20%",
+        "video" : ["coffee-plantation/Coffee Plantation_KM.mp4","coffee-plantation/Coffee_Plantation Path.mp4"],
+        "description" : `Terletak di kawasan perkebunan PTPN XII dan perkebunan rakyat
+                        di daerah pegunungan ljen-Raung di Kecamatan ljen dan Sumberwringin.
+
+                        Kabupaten Bondowoso merupakan sentra produksi Kopi Arabika (Java Coffee) terluas
+                        di Jawa Timur. Luas Perkebunan Kopi Bondowoso sekitar 7.054 Ha dan terletak
+                        di ketinggian sekitar 1100-1550 mdpl. 
+
+                        Letusan gunung api ljen Purba membentuk tanah di sekitar dataran berwarna kelabu kelam dan kaya akan unsur hara. Oleh sebab itu, Kopi Arabika Bondowoso merupakan satu-satunya produk Kopi Spesialti.
+
+                        (Kopi Blue Mountain) di Jawa Timur yang telah mendapatkan Sertifikat indikasi
+                        Geografis (IG). Berdasarkan hasil uji cita rasa bahwa kopi ini memiliki tingkat keasaman
+                        yang cukup tinggi, terjamin mutu, dan aroma khas yang kuat.
+
+                        Bondowoso Coffee Plantation Biosite is located to land of PTPN XII and Locat indigenous
+                        plantation in the len-Raung Highland of lien and Sumberwringin Subdistricts.
+
+                        Bondowoso Regency is the largest Arabica Coffee (Java Coffee) production center
+                        in East Java. The Biosite area is approximately 7,054 hectares at 1.100
+                        up to 1.550 meters above sea level. The volcanic eruption of en Purba constructed
+                        the soil color tumed to dark gray and fulfilled of nutrients.
+
+                        Therefore, Bondowoso Arabica Coffee is the only specialty coffee product (Blue Mountain Coffee) in East Java that has received Certificate of Geographical Indication (Gi). According to the result of Ravour evaluation, the categorization of the coffee has a high valued acidity and exceptional quality with a strong typical aroma.`,
+        "img" : ["1.jpg","2.jpg","3.jpg"],
+        "facilities" : [
+            {
+                "icon" : "fa fa-toilet",
+                "title" : "Toilet"
+            },
+        ],
+        "web360" : "kluncing/index.htm",
+        "rute" : ""
+    },
+    {
+        "name" : "Rainbow Forest",
+        "pinImg" : "Arak-Arak.png",
+        "position" : "left:50%;top:20%",
+        "video" : ["rainbow-forest/Rainbow Forest KM.mp4","rainbow-forest/Rainbow forest.mp4"],
+        "description" : `Rainbow Forest Biosite
+                114°0'9.77"E ; 7°59'56.58"'S
+                Biosite Hutan Pelangi terletak di Kecamatan Sumberwringin, Kabupaten Bondowoso.
+                Hutan Pelangi adalah Kawasan Hutan Dengan Tujuan Khusus (KHOTK) Sumberwringin seluas 23,6 Ha. 
+                sebagai pusat penelitian dan konservasi bagi beberapa jents kelompok tanaman sejak Tahun 1939. Nama Hutan Pelangi disesuaikan dengan karakteristik salah satu pohon eksotis yaitu Kayu Leda (Eucalyptus deglupta) yang tersebar di wilayah Maluku dan Papua. Daya tarik gradasi warna-warni seperti pelangi pada batang kayu disebabkan oleh proses Oksidasi Kambium batang dengan Oksigen dan menghasilkan warna hijau, kuning, biru, jingga hingga cokelat. Eucalyptus deglupta merupakan tanaman endemik Indonesia yang rentan dialam dan populasinya terus menurun akibat eksploitasi berlebihan di habitat aslinya.
+
+                Rainbow Forest Biosite is located in Sumberwringin Subdistrict, Bondowoso Regency. The Bicsite is determined as Sumberwringin Special Purpose Forest Area (Kawasan Hutan dengan Tujuan Khusus/KHDTK) since 1939.
+                it covers almost 23.6 hectares which belongs to the use of research and conservation center for some plant species.
+                Rainbow Forest is adjusted over the charactenstics of the specific one exotic tree that 1s known as Leda Wood (Eucalyptus Deglupta) which is spread in Maluku and Papua island. 
+                The attraction of colorful fogs gradation created by the oxidation process of the stem cambium with Oxygen that produces colors: green. yeliow, blue, orange. and brown. Eucalyptus Degiupta is an endemic plant which is naturally vulnerable that deals with decreasing population
+                numbers due to natural habitat overexploitation.`,
+        "img" : ["1.jpg","2.jpg","3.jpg"],
+        "facilities" : [
+            {
+                "icon" : "fa fa-toilet",
+                "title" : "Toilet"
+            },
+        ],
+        "web360" : "rainbow-forest/index.htm",
+        "rute" : "Rute dari Alun Alun Bondowoso, Kauman, Kotakulon, Kabupaten Bondowoso, Jawa Timur ke Hutan Pelangi Bondowoso.kml"
+    },
+    {
+        "name" : "Maskuning Kulon",
+        "pinImg" : "Arak-Arak.png",
+        "position" : "left:50%;top:-15%",
+        "video" : ["maskuning-kulon/Maskuning Kulon KM.mp4","maskuning-kulon/Maskuning Kulon Path_2.mp4"],
+        "description" : `113°53'15"E ; 7°58'39.97"S
+        Situs Megalltik Maskuning Kulon terfetak di Desa Maskuning Kulon, Kecamatan Pujer, Kabupaten Bondowoso.
+        Terdapat 90 benda Cagar Budaya yaitu Batu Silindris, Batu Dakon, Dolem yang letaknya berdekatan,
+        tertata rapi, dan mempunyai keunikan tersendiri.
+
+        Situs ini berbentuk komplek pemakaman yang terbuat dari batu berbentuk besar dengan kaki-kaki dibawahnya yang bertujuan agar jenazah tidak dimakan binatang buas atau dicuri orang, karena didalamnya terdapat bekal kubur berupa perhiasan dan pelaratan lain sebagai bekal menuju alam nenek moyang.
+
+        Maskuning Kulon Megalithic Site is located in Maskuning Kulon Village, Pujer Subdistrict, Bondowoso Regency.
+        There are over 90 objects of cultural heritage made of stone was found. for example, Cylindrical, Dakon, and Dolmen are placed closly nearby and straighly arranged with typical uniqueness.
+
+        The site is constructed such a burial complex that is made of large stones above the iegs which aim to protect
+        the corpse from wild animals or to be stolen, because there are many jewelry and other valuable thins as the provision to get through the ancestral realm.`,
+        "img" : ["1.jpg","2.jpg","3.jpg"],
+        "facilities" : [
+            {
+                "icon" : "fa fa-toilet",
+                "title" : "Toilet"
+            },
+        ],
+        "web360" : "maskuning-kulon/index.htm",
+        "rute" : "full-track-kompleks-dolmen-maskuning-kulon.kml"
+    },
+    {
+        "name" : "So'on Solor Stone Park",
+        "pinImg" : "Arak-Arak.png",
+        "position" : "left:25%;top:-10%",
+        "video" : ["solor/Solor KM.mp4","solor/Solor Path.mp4"],
+        "description" : `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?`,
+        "img" : ["1.jpg","2.jpg","3.jpg"],
+        "facilities" : [
+            {
+                "icon" : "fa fa-toilet",
+                "title" : "Toilet"
+            },
+        ],
+        "web360" : "solor/index.htm",
+        "rute" : "Rute dari Alun Alun Bondowoso, Kauman, Kotakulon, Kabupaten Bondowoso, Jawa Timur ke Batu So'on Solor.kml"
+    },
+    {
+        "name" : "Butha Cave Structure Of Cermee",
+        "pinImg" : "Arak-Arak.png",
+        "position" : "left:45%;top:-10%",
+        "video" : ["butha/Butha Cave Structure of Cermee Path.mp4"],
+        "description" : `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?`,
+        "img" : ["1.jpg","2.jpg","3.jpg"],
+        "facilities" : [
+            {
+                "icon" : "fa fa-toilet",
+                "title" : "Toilet"
+            },
+        ],
+        "web360" : "butha/index.htm",
+        "rute" : "Rute dari Alun Alun Bondowoso, Kauman, Kotakulon, Kabupaten Bondowoso, Jawa Timur ke Butha Cave Structure of Cermee.kml"
+    },
+    {
+        "name" : "Butha Cave Structure Of Sumber Canting",
+        "pinImg" : "Arak-Arak.png",
+        "position" : "left:30%;top:-10%",
+        "video" : ["butha-canting/Butha Cave Structure of Sumber Canting Path.mp4"],
+        "description" : `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?`,
+        "img" : ["1.jpg","2.jpg","3.jpg"],
+        "facilities" : [
+            {
+                "icon" : "fa fa-toilet",
+                "title" : "Toilet"
+            },
+        ],
+        "web360" : "butha-canting/index.htm",
+        "rute" : "RUTEDA_1.KML"
+    },
+    {
+        "name" : "Kalipait Acidic Stream",
+        "pinImg" : "Arak-Arak.png",
+        "position" : "left:60%;top:6%",
+        "video" : ["kalipait/Kalipait Acidic Stream PATH.mp4"],
+        "description" : `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi velit, consectetur necessitatibus amet deleniti dignissimos tempore laborum nesciunt molestiae? Perspiciatis, eius dicta tempore deserunt ad ratione animi magni officiis illum?`,
+        "img" : ["1.jpg","2.jpg","3.jpg"],
+        "facilities" : [
+            {
+                "icon" : "fa fa-toilet",
+                "title" : "Toilet"
+            },
+        ],
+        "web360" : "kalipait/index.htm",
+        "rute" : "Rute dari Alun Alun Bondowoso, Kauman, Kotakulon, Kabupaten Bondowoso, Jawa Timur ke Kalipait Sulphur Water Fall, Curah Macan, Kalianyar, Kabupaten Bondowoso, Jawa .kml"
+    },
+    {
+        "name" : "Kawah Wurung Tour",
+        "pinImg" : "Arak-Arak.png",
+        "position" : "left:40%;top:20%",
+        "video" : ["kawah-wurung/Kawah Wurung.mp4"],
+        "description" : `114°09'54.03"E ; 8°04'02.08"S
+            Geosite Kawah Wurung terletak di antara perbatasan Desa Jampit dan Desa Kalianyar,
+            Kecamatan ljen, Kabupaten Bondowoso.
+
+            Kawasan Geosite ini terbentuk dari aktivitas gunung api yang kompleks.
+            Di sekitar kawasan Geosite Kawah Wurung terdapat Gunung Api Intra Kaldera ljen yang terdiri atas; Gunung Kawah Wurung, G. Genteng, G. Pendil, G. Pendtan, dan G. Anyar.
+
+            Geosite Kawah Wurung mempunyai nilai keunggulan Geologi berupa tampilan Morfologi Gunung Api ‘Teletubbies’ Intra Kaldera. Keragaman satuan batuan penyusun kawasan Geosite ini disebabkan oleh sumber material dari vulkanisme Gunung Api Intra Kaldera maupun Ring Kaldera terdekat, sekaligus berpengaruh terahdap mineral petrografi penyusun batuan.
+
+
+            Wurung Crater Geosite is located between the borders of Jampit and Kalianyar Village,
+
+            Ijen Subdistrict, Bondowoso Regency. This Geosite area is formed from complex volcanic
+            activities. Around the Geosite area formerly exists Ijen intra Caldera Voicano which consists
+            of; Mount Wurung Crater, Mt. Genteng, Mt. Pendil, Mt. Pendian, and Mt. Anyar.
+
+            The Geosite considers a geological significance of volcanic morphology ‘Teletubbies' intra caldera.
+            The diversity of Wurung Crater Geosite area is occured from the volcanism material sources of Intra Caldera Volcano and Caldera Ring, and affected as well through Petrographic Mineral of materials construction.`,
+        "img" : ["1.jpg","2.jpg","3.jpg"],
+        "facilities" : [
+            {
+                "icon" : "fa fa-toilet",
+                "title" : "Toilet"
+            },
+        ],
+        "web360" : "kawah-wurung/index.htm",
+        "rute" : "Rute dari Alun Alun Bondowoso, Kauman, Kotakulon, Kabupaten Bondowoso, Jawa Timur ke Kalianyar, Sempol, Margahayu, Kalianyar, Sempol, Kabupaten Bondowoso, Jawa Timur 68288, Indones.kml"
+    },
+]
